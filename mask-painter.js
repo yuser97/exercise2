9963137f529fbab213e09053c09a9c3c40c60ce3
Yury Sergeyev <60914627+yuser97@users.noreply.github.com>
2025-04-13 00:21:38 +0300

    Update mask-painter.js

--- mask-painter.js
+++ mask-painter.js
@@ -3,18 +3,14 @@ registerPaint('maskPainter', class {
     static get inputProperties() { return ['--hole-positions']; }
     
     paint(ctx, size, properties) {
-        // Заливаем черным (это будет непрозрачная часть маски)
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, size.width, size.height);
         
-        // Делаем дыры прозрачными
         ctx.globalCompositeOperation = 'destination-out';
         ctx.fillStyle = 'white';
         
-        // Получаем позиции дыр из CSS переменной
         const holePositions = JSON.parse(properties.get('--hole-positions').toString() || '[]');
         
-        // Рисуем прозрачные прямоугольники в указанных позициях
         holePositions.forEach(hole => {
             ctx.fillRect(hole.x, hole.y, hole.width, hole.height);
         });
